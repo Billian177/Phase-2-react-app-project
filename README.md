1095ade3353dc6448f27d730347531283babb8c1
bill apiyo <billian177@gmail.com>
2022-07-31 14:45:27 +0300

    edited readme file

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
+# Phase-2-React-Project
+
+# Bible Quote.
+
+This is an app that allows one to display Bible reference scriptures and be able to view quotes. This is a graded project to certify my certificate in software development. This is based on knowledge learnt in react, phase 2 of the coursework.
+
+Author: Bill Ian Apiyo
+Title: Student at Moringa School
+Powered by: Moringa School
+
+# About Bible Quote.
+It is an one page web
+It fetches data from two API's i.e Bible API and Quotes API
+It is based on react components and routing knowledge
+It allows user to add/post a scripture or bible quote through forms
+User is able to learn some other non-biblical quotes and like on favourite quotes
+Favourite quotes can be seen in the other side of the page
+
+# React Components
+App.js
+NewScriptureForm.js
+Quotes.js
+Scripture.js
+ScriptureContainer.js
+
+## License
+
+Copyright (c) 2022 **Bill Apiyo**
+
+Permission is hereby granted free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify and merge.
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PoemsContainer from "./PoemsContainer";
+import NewPoemForm from "./NewPoemForm";
+
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [formVisible, setFormVisible] = useState(true);
+  const [favoriteVisible, setFavoriteVisible] = useState(true);
+  const poemsToDisplay = poems.filter((poem) => favoriteVisible || poem.isFavorite);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then(res => res.json())
+      .then(data => setQuotes(data))
+  }, []);
+
+  function addQuotes(newQuote) {
+    setPoems([...quotes, newQuote]);
+  }
+
+
+  function addToFavorites(favPoem) {
+    setPoems(poems.map(poem => {
+      return poem.id === favPoem.id ? {...favPoem, isFavorite: !favPoem.isFavorite} : poem
+      }  
+    ))
+  }
+
+  function renderPoemView() {
+    if (poemsToDisplay.length === 0 && !favoriteVisible) {
+      return (<h1>You have no favorites added</h1>)
+    } else {
+      return (
+        <PoemsContainer 
+          poems={poemsToDisplay}
+          addToFavorites={addToFavorites}
+        />
+      )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <button 
+          onClick={() => setFormVisible(!formVisible)} >
+          Show/hide new poem form
+        </button>
+        {formVisible ? <NewPoemForm addPoem={addPoem} /> : null}
+
+        <button onClick={() => setFavoriteVisible(!favoriteVisible)} >
+          Show/hide Favorite Poems
+        </button>
+      </div>
+      {renderPoemView()}
     </div>
   );
 }
-
-export default App;
